b33851bcdc562b88e40bf99b36edbfba77512c83
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2021-07-20 00:47:00 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Syun Tanaka on 2021/07/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
